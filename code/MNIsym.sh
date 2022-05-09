a64fc43b47fdc7af016c509e2d6ef75b93e52f4c
Leonardo Muller-Rodriguez <leomullerrodri@head1.htc.inm7.de>
2022-05-09 11:54:54 +0200

    Save code script to git

--- code/MNIsym.sh
+++ code/MNIsym.sh
@@ -1,1 +1,5 @@
-../.git/annex/objects/26/k0/MD5E-s229--08709b6ef68011c782c053eabde8ed19.sh/MD5E-s229--08709b6ef68011c782c053eabde8ed19.sh
+#!/bin/sh
+for i in {01..21}; do
+fsl5.0-applywarp --premat=$0 -i $1 -o ./transforms/sub-"$i"/sub"$i"_MNIsymbrain.nii.gz -r $2 -w $3
+fsl5.0-slicetimer -i ./transforms/sub-"$i"/sub"$i"_MNIsymbrain.nii.gz -o ./outputs/sub"$i"_STC_MNIsymbrain.nii.gz -r 2
+datalad save -m "sub-$i cont-MNIsym brain parcellation"; done
